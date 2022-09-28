8823f64199d83852d02e6120d5b5ecbe8cc583d0
Maltsev Nikita <maltsew.na@gmail.com>
2022-09-28 11:34:51 -0400

    register models in admin site for testing development

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Author, Book, Genre, Language, Status, BookInstance
 
 # Register your models here.
+# admin.site.register(Author)
+# admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+# admin.site.register(BookInstance)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_genre', 'display_language', 'display_author')
+    list_filter = ('genre', 'author')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('book', 'status')
