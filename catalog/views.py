1ce545cafcb3ace35c70d1e4467026a46e22ddd8
Maltsev Nikita <maltsew.na@gmail.com>
2022-10-04 18:42:50 +0300

    add a counter of visiting main page by every user with using request.session

--- catalog/views.py
+++ catalog/views.py
@@ -12,11 +12,16 @@ def index(requset):
 
     num_author = Author.objects.all().count()
 
+    # count of visits this view, counted with var "session"
+    num_visits = requset.session.get('num_visits', 0)
+    requset.session['num_visits'] = num_visits + 1
+
     return TemplateResponse(requset, 'index.html',
                             context={'num_books': num_books,
                                      'num_instances': num_instances,
                                      'num_instances_available': num_instances_available,
-                                     'num_author': num_author},
+                                     'num_author': num_author,
+                                     'num_visits': num_visits},
                             )
 
 
