edbd205b8e46af63645f1846611e5fa8bdd6dfca
Maltsev Nikita <maltsew.na@gmail.com>
2022-10-18 20:25:40 +0300

    add folder for tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from catalog.views import index
+from catalog.models import Genre
 
-# Create your tests here.
+
+class MainPageTest(TestCase):
+    """"""
+
+    def test_root_url_resolves_main_page(self):
+        """Test unites: checking root url "/" to compliance index view and
+         index view compliance index.html template"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
